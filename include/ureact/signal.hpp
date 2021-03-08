f8fc7cb82ba5df83a7414b2b72c52b9035114969
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-03-08 05:48:24 +0300

    signal_pack is moved to a separate file

--- include/ureact/signal.hpp
+++ include/ureact/signal.hpp
@@ -5,6 +5,7 @@
 #include <type_traits>
 #include <utility>
 
+#include "ureact/signal_pack.hpp"
 #include "ureact/context.hpp"
 #include "ureact/observer.hpp"
 #include "ureact/type_traits.hpp"
@@ -27,34 +28,6 @@ class var_signal;
 
 template <typename S, typename op_t>
 class temp_signal;
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-/// signal_pack - Wraps several nodes in a tuple. Create with comma operator.
-///////////////////////////////////////////////////////////////////////////////////////////////////
-template <typename... values_t>
-class signal_pack
-{
-public:
-    signal_pack( const signal<values_t>&... deps )
-        : data( std::tie( deps... ) )
-    {}
-
-    template <typename... cur_values_t, typename append_value_t>
-    signal_pack( const signal_pack<cur_values_t...>& cur_args, const signal<append_value_t>& new_arg )
-        : data( std::tuple_cat( cur_args.data, std::tie( new_arg ) ) )
-    {}
-
-    std::tuple<const signal<values_t>&...> data;
-};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-/// with - Utility function to create a signal_pack
-///////////////////////////////////////////////////////////////////////////////////////////////////
-template <typename... values_t>
-auto with( const signal<values_t>&... deps ) -> signal_pack<values_t...>
-{
-    return signal_pack<values_t...>( deps... );
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /// make_var
@@ -163,26 +136,6 @@ auto make_signal(const signal_pack<values_t...>& arg_pack, in_f&& func)
     return apply(
         node_builder( std::get<0>(arg_pack.data).get_context(), std::forward<in_f>(func) ),
         arg_pack.data);
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-/// Comma operator overload to create signal pack from 2 signals.
-///////////////////////////////////////////////////////////////////////////////////////////////////
-template <typename left_val_t, typename right_val_t>
-auto operator,( const signal<left_val_t>& a, const signal<right_val_t>& b )
-    -> signal_pack<left_val_t, right_val_t>
-{
-    return signal_pack<left_val_t, right_val_t>( a, b );
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-/// Comma operator overload to append node to existing signal pack.
-///////////////////////////////////////////////////////////////////////////////////////////////////
-template <typename... cur_values_t, typename append_value_t>
-auto operator,( const signal_pack<cur_values_t...>& cur, const signal<append_value_t>& append )
-    -> signal_pack<cur_values_t..., append_value_t>
-{
-    return signal_pack<cur_values_t..., append_value_t>( cur, append );
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--- include/ureact/signal_.hpp
+++ include/ureact/signal_.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "ureact/detail/signal_base.hpp"
+
 namespace ureact
 {
 

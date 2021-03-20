e0bd1f3b58c773170d4397f3876b6b2e8b323de6
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-03-20 14:58:49 +0300

    signal code is refactored

--- include/ureact/detail/signal.hpp
+++ include/ureact/detail/signal.hpp
@@ -11,50 +11,24 @@ namespace ureact
 /// signal
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename S>
-class signal : public ::ureact::detail::signal_base<S>
+class signal : public detail::signal_base<S>
 {
 private:
-    using node_t = ::ureact::detail::signal_node<S>;
+    using node_t = detail::signal_node<S>;
     using node_ptr_t = std::shared_ptr<node_t>;
 
 public:
     using value_t = S;
 
-    // Default ctor
     signal() = default;
 
-    // Copy ctor
-    signal( const signal& ) = default;
-
-    // Move ctor
-    signal( signal&& other ) noexcept = default;
-
-    // Node ctor
     explicit signal( node_ptr_t&& node_ptr )
         : signal::signal_base( std::move( node_ptr ) )
     {}
 
-    // Copy assignment
-    signal& operator=( const signal& ) = default;
-
-    // Move assignment
-    signal& operator=( signal&& other ) noexcept = default;
-
-    ~signal() = default;
-
     const S& value() const
     {
         return signal::signal_base::get_value();
-    }
-
-    bool equals( const signal& other ) const
-    {
-        return signal::signal_base::equals( other );
-    }
-
-    bool is_valid() const
-    {
-        return signal::signal_base::is_valid();
     }
 
     S flatten() const
@@ -66,56 +40,24 @@ public:
 
 // Specialize for references
 template <typename S>
-class signal<S&> : public ::ureact::detail::signal_base<std::reference_wrapper<S>>
+class signal<S&> : public detail::signal_base<std::reference_wrapper<S>>
 {
 private:
-    using node_t = ::ureact::detail::signal_node<std::reference_wrapper<S>>;
+    using node_t = detail::signal_node<std::reference_wrapper<S>>;
     using node_ptr_t = std::shared_ptr<node_t>;
 
 public:
     using value_t = S;
 
-    // Default ctor
     signal() = default;
 
-    // Copy ctor
-    signal( const signal& ) = default;
-
-    // Move ctor
-    signal( signal&& other ) noexcept
-        : signal::signal_base( std::move( other ) )
-    {}
-
-    // Node ctor
     explicit signal( node_ptr_t&& node_ptr )
         : signal::signal_base( std::move( node_ptr ) )
     {}
 
-    // Copy assignment
-    signal& operator=( const signal& ) = default;
-
-    // Move assignment
-    signal& operator=( signal&& other ) noexcept
-    {
-        signal::signal_base::operator=( std::move( other ) );
-        return *this;
-    }
-
-    ~signal() = default;
-
     const S& value() const
     {
         return signal::signal_base::get_value();
-    }
-
-    bool equals( const signal& other ) const
-    {
-        return signal::signal_base::equals( other );
-    }
-
-    bool is_valid() const
-    {
-        return signal::signal_base::is_valid();
     }
 };
 

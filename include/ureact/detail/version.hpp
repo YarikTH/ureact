d0afba0dafef95ab8b5cd93f3cf80732dbfff82b
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-03-21 12:56:55 +0300

    Version header is added. So it is officially first ureact version

---
+++ include/ureact/detail/version.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+// The ureact library version in the form major * 10000 + minor * 100 + patch.
+#define UREACT_VERSION 00100
--- include/ureact/ureact.hpp
+++ include/ureact/ureact.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ureact/detail/version.hpp"
 #include "ureact/detail/signal.hpp"
 #include "ureact/detail/context.hpp"
 #include "ureact/detail/observer.hpp"

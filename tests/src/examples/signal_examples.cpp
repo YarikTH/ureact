4d7d79ff0cf7ba71612d1b76a75b3760bc9906a1
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-05-03 21:33:58 +0300

    #26 changing_multiple_inputs and reacting_to_value_changes examples are converted into tests

--- tests/src/examples/signal_examples.cpp
+++ tests/src/examples/signal_examples.cpp
@@ -64,4 +64,64 @@ TEST_SUITE( "Examples" )
 
         CHECK( data.value() == std::vector<std::string>{ "Hello", "World!" } );
     }
+
+    TEST_CASE( "Changing multiple inputs" )
+    {
+        ureact::context ctx;
+
+        ureact::var_signal<int> a = make_var( ctx, 1 );
+        ureact::var_signal<int> b = make_var( ctx, 1 );
+
+        ureact::signal<int> x = a + b;
+        ureact::signal<int> y = a + b;
+        ureact::signal<int> z = x + y; // 2*(a+b)
+
+        //  a b  //
+        //  /X\  //
+        // x   y //
+        //  \ /  //
+        //   z   //
+
+        std::vector<int> z_values;
+        observe( z, [&]( int new_value ) { z_values.push_back( new_value ); } );
+
+        CHECK( z.value() == 4 );
+        CHECK( z_values == std::vector<int>{} );
+
+        a <<= 2; // z is changed to 6
+        b <<= 2; // z is changed to 8
+
+        ctx.do_transaction( [&] {
+            a <<= 4;
+            b <<= 4;
+        } ); // z is changed to 16
+
+        CHECK( z_values == std::vector<int>{ 6, 8, 16 } );
+    }
+
+    TEST_CASE( "Reacting to value changes" )
+    {
+        ureact::context ctx;
+
+        ureact::var_signal<int> x = make_var( ctx, 1 );
+        ureact::signal<int> xAbs = make_signal( x, []( int value ) { return abs( value ); } );
+
+        std::vector<int> xAbs_values;
+        observe( xAbs, [&]( int new_value ) { xAbs_values.push_back( new_value ); } );
+
+        CHECK( xAbs.value() == 1 );
+        CHECK( xAbs_values == std::vector<int>{} );
+
+        x <<= 2;  // xAbs is changed to 2
+        x <<= -3; // xAbs is changed to 3
+        x <<= 3;  // no output, xAbs is still 3
+
+        ctx.do_transaction( [&] {
+            x <<= 4;
+            x <<= -2;
+            x <<= 3;
+        } ); // no output, result value of xAbs is still 3
+
+        CHECK( xAbs_values == std::vector<int>{ 2, 3 } );
+    }
 }

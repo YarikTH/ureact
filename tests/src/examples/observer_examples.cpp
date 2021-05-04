fb97d90dfa970528a5db7f20d6d9fbe48b9a90cb
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2021-05-04 04:01:21 +0300

    #26 observer examples are converted into tests

--- tests/src/examples/observer_examples.cpp
+++ tests/src/examples/observer_examples.cpp
@@ -3,11 +3,11 @@
 
 TEST_SUITE( "Examples" )
 {
-    TEST_CASE( "Subject-bound observers" )
+    TEST_CASE( "Observers" )
     {
         ureact::context ctx;
 
-        auto x = make_var( ctx, 1 );
+        auto x = make_var( ctx, 0 );
 
         auto identity = []( const int value ) { return value; };
 
@@ -17,9 +17,9 @@ TEST_SUITE( "Examples" )
 
         CHECK( x_values == std::vector<int>{} );
 
-        SUBCASE( "v1" )
+        SUBCASE( "Subject-bound observers v1" )
         {
-            // my_signal scope
+            // Inner scope
             {
                 // Create a signal in the function scope
                 auto my_signal = make_signal( x, identity );
@@ -28,18 +28,18 @@ TEST_SUITE( "Examples" )
                 // After scope my_signal is destroyed, and so is the observer
                 observe( my_signal, on_x_value_change );
 
-                x <<= 2; // output: 2
+                x <<= 1; // output: 2
 
-                CHECK( x_values == std::vector<int>{ 2 } );
+                CHECK( x_values == std::vector<int>{ 1 } );
             }
-            // ~my_signal scope
+            // ~Inner scope
 
-            x <<= 3; // no ouput
+            x <<= 2; // no ouput
 
-            CHECK( x_values == std::vector<int>{ 2 } );
+            CHECK( x_values == std::vector<int>{ 1 } );
         }
 
-        SUBCASE( "v2" )
+        SUBCASE( "Subject-bound observers v2" )
         {
             // Outer scope
             {
@@ -55,18 +55,18 @@ TEST_SUITE( "Examples" )
 
                     // The node linked to my_signal is now also owned by obs
 
-                    x <<= 2; // output: 2
+                    x <<= 1; // output: 2
 
-                    CHECK( x_values == std::vector<int>{ 2 } );
+                    CHECK( x_values == std::vector<int>{ 1 } );
                 }
                 // ~Inner scope
 
                 // my_signal was destroyed, but as long as obs exists and is still
                 // attached to the signal node, this signal node won't be destroyed
 
-                x <<= 3; // output: 3
+                x <<= 2; // output: 3
 
-                CHECK( x_values == std::vector<int>{ 2, 3 } );
+                CHECK( x_values == std::vector<int>{ 1, 2 } );
             }
             // ~Outer scope
 
@@ -74,9 +74,76 @@ TEST_SUITE( "Examples" )
             // -> the signal node is no longer owned by anything and is destroyed
             // -> the observer node is destroyed as it was bound to the subject
 
-            x <<= 4; // no ouput
+            x <<= 3; // no ouput
 
-            CHECK( x_values == std::vector<int>{ 2, 3 } );
+            CHECK( x_values == std::vector<int>{ 1, 2 } );
+        }
+
+        SUBCASE( "Detaching observers manually" )
+        {
+            ureact::observer obs = observe( x, on_x_value_change );
+
+            x <<= 1; // output: 2
+
+            CHECK( x_values == std::vector<int>{ 1 } );
+
+            obs.detach(); // detach the observer
+
+            x <<= 2; // no output
+
+            CHECK( x_values == std::vector<int>{ 1 } );
+        }
+
+        SUBCASE( "Using scoped observer" )
+        {
+            // Note the semantic difference between scoped_observer and observer.
+            //
+            // During its lifetime, the observer handle of an observer guarantees that the
+            // observed subject will not be destroyed and allows explicit detach.
+            // But even after the observer handle is destroyed, the subject may continue to exist
+            // and so will the observer.
+            //
+            // scoped_observer has similar semantics to a scoped lock.
+            // When it's destroyed, it detaches and destroys the observer.
+
+            // Inner scope
+            {
+                ureact::scoped_observer scoped_obs = observe( x, on_x_value_change );
+
+                x <<= 1; // output: 1
+
+                CHECK( x_values == std::vector<int>{ 1 } );
+            }
+            // ~Inner scope
+
+            x <<= 2; // no output
+
+            CHECK( x_values == std::vector<int>{ 1 } );
+        }
+
+        SUBCASE( "Detaching observers using return value" )
+        {
+            // Functor used for observer can optionally return value
+            // Using this value observer can be optionally self detached
+            ureact::observer obs = observe( x, [&]( const int v ) {
+                if( v < 0 )
+                {
+                    return ureact::observer_action::stop_and_detach;
+                }
+                else
+                {
+                    x_values.push_back( v );
+                    return ureact::observer_action::next;
+                }
+            } );
+
+            x <<= 1;
+            x <<= 2;
+            x <<= 3;
+            x <<= -1;
+            x <<= 4;
+
+            CHECK( x_values == std::vector<int>{ 1, 2, 3 } );
         }
     }
 }

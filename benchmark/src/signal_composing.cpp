23a20bafedaba57d27f49709ecefbdbe687c5b3c
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-06-14 12:11:16 +0300

    Add benchmark

---
+++ benchmark/src/signal_composing.cpp
@@ -0,0 +1,81 @@
+#include <nanobench.h>
+
+#include "ureact/ureact.hpp"
+
+namespace
+{
+
+void perform_test( const char* name,
+    ankerl::nanobench::Bench& bench,
+    const ureact::var_signal<int>& in,
+    const ureact::signal<int>& out )
+{
+    assert( out.get() == 8 );
+
+    int i = 0;
+
+    bench.run( name, [&] {
+        in <<= i;
+        assert( out.get() == 8 * i * i );
+        bench.doNotOptimizeAway( out.get() );
+        ++i;
+    } );
+}
+
+// Take signal constructed with lambda expression as a baseline
+void signal_functions_baseline( ankerl::nanobench::Bench& bench )
+{
+    ureact::context ctx;
+    auto a = make_var( ctx, 1 );
+    auto d = make_signal( a,
+        []( int a ) { //
+            return ( ( a + a ) * ( a + a ) ) + ( ( a + a ) * ( a + a ) );
+        } );
+
+    perform_test( "baseline", bench, a, d );
+}
+
+// Signal constructed from the expression. Intermediate signals should be optimized by ureact
+void signal_functions_expression( ankerl::nanobench::Bench& bench )
+{
+    ureact::context ctx;
+    auto a = make_var( ctx, 1 );
+    auto d = ( ( a + a ) * ( a + a ) ) + ( ( a + a ) * ( a + a ) );
+
+    perform_test( "expression", bench, a, d );
+}
+
+// Make a separate signal for each calculation step and store them into variable
+// to make them L-value, so they can't be optimized by ureact
+void signal_functions_separate( ankerl::nanobench::Bench& bench )
+{
+    ureact::context ctx;
+    auto a = make_var( ctx, 1 );
+
+    auto b1 = make_signal( with( a, a ), std::plus<>() );
+    auto b2 = make_signal( with( a, a ), std::plus<>() );
+    auto b3 = make_signal( with( a, a ), std::plus<>() );
+    auto b4 = make_signal( with( a, a ), std::plus<>() );
+
+    auto c1 = make_signal( with( b1, b2 ), std::multiplies<>() );
+    auto c2 = make_signal( with( b3, b4 ), std::multiplies<>() );
+
+    auto d = make_signal( with( c1, c2 ), std::plus<>() );
+
+    perform_test( "separate", bench, a, d );
+}
+
+} // namespace
+
+int main()
+{
+    ankerl::nanobench::Bench b;
+    b.title( "Signal composing comparison" );
+    b.warmup( 100 );
+    b.relative( true );
+    b.performanceCounters( false );
+
+    signal_functions_baseline( b );
+    signal_functions_expression( b );
+    signal_functions_separate( b );
+}

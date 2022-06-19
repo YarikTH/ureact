d27655dc5c115f6a3d12ec42fb9e24cf0d6af955
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-06-19 07:57:12 +0300

    move move_test.cpp to main.cpp

--- tests/src/include/tests_stdafx.hpp
+++ tests/src/include/tests_stdafx.hpp
@@ -82,3 +82,68 @@ auto make_deeper( T&& signal )
         result = +result;
     return result;
 }
+
+struct copy_stats
+{
+    int copy_count = 0;
+    int move_count = 0;
+};
+
+struct copy_counter
+{
+    int v = 0;
+    copy_stats* stats = nullptr;
+
+    copy_counter() = default;
+
+    copy_counter( int x, copy_stats* s )
+        : v( x )
+        , stats( s )
+    {}
+
+    copy_counter( const copy_counter& other )
+        : v( other.v )
+        , stats( other.stats )
+    {
+        stats->copy_count++;
+    }
+
+    copy_counter( copy_counter&& other ) noexcept
+        : v( other.v )
+        , stats( other.stats )
+    {
+        stats->move_count++;
+    }
+
+    copy_counter& operator=( const copy_counter& other )
+    {
+        if( &other != this )
+        {
+            v = other.v;
+            stats = other.stats;
+            stats->copy_count++;
+        }
+        return *this;
+    }
+
+    copy_counter& operator=( copy_counter&& other ) noexcept
+    {
+        if( &other != this )
+        {
+            v = other.v;
+            stats = other.stats;
+            stats->move_count++;
+        }
+        return *this;
+    }
+
+    copy_counter operator+( const copy_counter& r ) const
+    {
+        return copy_counter{ v + r.v, stats };
+    }
+
+    bool operator==( const copy_counter& other ) const
+    {
+        return v == other.v;
+    }
+};
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -11,6 +11,35 @@
 #include "tests_stdafx.hpp"
 #include "ureact/ureact.hpp"
 
+TEST_CASE( "CopyStatsForSignalCalculations" )
+{
+    ureact::context ctx;
+
+    copy_stats stats;
+
+    auto a = ureact::make_var( ctx, copy_counter{ 1, &stats } );
+    auto b = ureact::make_var( ctx, copy_counter{ 10, &stats } );
+    auto c = ureact::make_var( ctx, copy_counter{ 100, &stats } );
+    auto d = ureact::make_var( ctx, copy_counter{ 1000, &stats } );
+
+    // 4x move to m_value_
+    // 4x copy to m_new_value (can't be uninitialized for references)
+    CHECK( stats.copy_count == 4 );
+    CHECK( stats.move_count == 4 );
+
+    auto x = a + b + c + d;
+
+    CHECK( stats.copy_count == 4 );
+    CHECK( stats.move_count == 7 );
+    CHECK( x.get().v == 1111 );
+
+    a <<= copy_counter{ 2, &stats };
+
+    CHECK( stats.copy_count == 4 );
+    CHECK( stats.move_count == 10 );
+    CHECK( x.get().v == 1112 );
+}
+
 // filter only even integer events
 // our check function depends only on event value
 TEST_CASE( "Filter" )

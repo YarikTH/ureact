74356e5fa0099d507d8a619d205bd90db62f1eb5
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-07-16 17:45:37 +0300

    add tests for count() algorithm

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1442,6 +1442,33 @@ TEST_CASE( "Unique" )
     CHECK( result.get() == expected );
 }
 
+// Counts amount of received events into signal<S>
+TEST_CASE( "Count" )
+{
+    ureact::context ctx;
+
+    auto src = ureact::make_source<>( ctx );
+    ureact::signal<size_t> counter;
+    ureact::signal<int> integer_counter;
+
+    SUBCASE( "Functional syntax" )
+    {
+        counter = ureact::count( src );              // Default version creates signal<size_t>
+        integer_counter = ureact::count<int>( src ); // Type of signal can be explicitly requested
+    }
+    SUBCASE( "Piped syntax" )
+    {
+        counter = src | ureact::count();
+        integer_counter = src | ureact::count<int>();
+    }
+
+    src.emit();
+    src.emit();
+
+    CHECK( counter.get() == 2 );
+    CHECK( integer_counter.get() == 2 );
+}
+
 // hold the last event value as a signal
 TEST_CASE( "Hold" )
 {

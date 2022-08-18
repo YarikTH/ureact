5359db44214947463fd0430980b92764b76a3ef8
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-08-18 04:11:10 +0300

    add missing license boilerplate

--- tests/src/lift.cpp
+++ tests/src/lift.cpp
@@ -1,2 +1,9 @@
+//
+//         Copyright (C) 2020-2022 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
 #include "doctest_extra.h"
 #include "ureact/ureact.hpp"

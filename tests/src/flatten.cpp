4dda713904cf4ed61f264c20984315af187cfe1a
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-09-03 07:54:54 +0300

    move "flatten" algorithm into separate header

--- tests/src/flatten.cpp
+++ tests/src/flatten.cpp
@@ -5,12 +5,13 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
+#include "ureact/flatten.hpp"
+
 #include <algorithm>
 #include <queue>
 #include <variant>
 
 #include "doctest_extra.h"
-#include "ureact/ureact.hpp"
 
 // TODO: replace FlattenN tests with more meaningful ones
 

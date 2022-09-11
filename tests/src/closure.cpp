db1cb9c4c83472df19504a6d4a22b0357b02a679
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2022-09-11 14:03:38 +0300

    add file for closure tests

---
+++ tests/src/closure.cpp
@@ -0,0 +1,11 @@
+//
+//         Copyright (C) 2020-2022 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "doctest_extra.h"
+#include "ureact/ureact.hpp"
+
+// TODO: test combinations of closure objects. Call with R-value and L-value closure objects

01fe01f258887238ed106f5226b7aefbad39d7a3
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-02-25 00:38:26 +0300

    add "elements", "keys" and "values" adaptors

---
+++ include/ureact/adaptor/elements.hpp
@@ -0,0 +1,43 @@
+//
+//         Copyright (C) 2014-2017 Sebastian Jeckel.
+//         Copyright (C) 2020-2023 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef UREACT_ADAPTOR_ELEMENTS_HPP
+#define UREACT_ADAPTOR_ELEMENTS_HPP
+
+#include <tuple>
+
+#include <ureact/adaptor/transform.hpp>
+
+UREACT_BEGIN_NAMESPACE
+
+/*!
+ * @brief Takes event stream of tuple-like values and creates a new event stream with a value-type of the N-th element of received value-type
+ * 
+ *  For every event e in source, emit t = std::get<N>(e).
+ */
+template <size_t N>
+inline constexpr auto elements = transform( []( const auto& e ) { return std::get<N>( e ); } );
+
+/*!
+ * @brief Takes event stream of tuple-like values and creates a new event stream with a value-type of the first element of received value-type
+ * 
+ *  For every event e in source, emit t = std::get<0>(e).
+ */
+inline constexpr auto keys = elements<0>;
+
+/*!
+ * @brief Takes event stream of tuple-like values and creates a new event stream with a value-type of the second element of received value-type
+ * 
+ *  For every event e in source, emit t = std::get<1>(e).
+ */
+inline constexpr auto values = elements<1>;
+
+UREACT_END_NAMESPACE
+
+#endif // UREACT_ADAPTOR_ELEMENTS_HPP
---
+++ tests/src/elements.cpp
@@ -0,0 +1,55 @@
+//
+//         Copyright (C) 2020-2023 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "ureact/adaptor/elements.hpp"
+
+#include "doctest_extra.h"
+#include "ureact/adaptor/collect.hpp"
+#include "ureact/events.hpp"
+
+// Based on example on https://en.cppreference.com/w/cpp/ranges/elements_view
+TEST_CASE( "Elements" )
+{
+    ureact::context ctx;
+
+    auto src = ureact::make_source<std::tuple<int, char, std::string>>( ctx );
+    ureact::events<int> ints;
+    ureact::events<char> chars;
+    ureact::events<std::string> strings;
+
+    SUBCASE( "Functional syntax" )
+    {
+        ints = ureact::keys( src );
+        chars = ureact::values( src );
+        strings = ureact::elements<2>( src );
+    }
+    SUBCASE( "Piped syntax" )
+    {
+        ints = src | ureact::keys;
+        chars = src | ureact::values;
+        strings = src | ureact::elements<2>;
+    }
+
+    const auto ints_values = ureact::collect<std::vector>( ints );
+    const auto chars_values = ureact::collect<std::vector>( chars );
+    const auto strings_values = ureact::collect<std::vector>( strings );
+
+    const std::vector<std::tuple<int, char, std::string>> vt{
+        { 1, 'A', "α" },
+        { 2, 'B', "β" },
+        { 3, 'C', "γ" },
+        { 4, 'D', "δ" },
+        { 5, 'E', "ε" },
+    };
+    std::copy( vt.begin(), vt.end(), src.begin() );
+
+    // clang-format off
+    CHECK( ints_values.get()    == std::vector<int>{          1,   2,   3,   4,   5  } );
+    CHECK( chars_values.get()   == std::vector<char>{        'A', 'B', 'C', 'D', 'E' } );
+    CHECK( strings_values.get() == std::vector<std::string>{ "α", "β", "γ", "δ", "ε" } );
+    // clang-format on
+}

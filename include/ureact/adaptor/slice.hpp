5f432a9b4405b1609ea217c0800438f83a5c36dc
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-02-26 14:14:43 +0300

    add SliceAdaptor

---
+++ include/ureact/adaptor/slice.hpp
@@ -0,0 +1,65 @@
+//
+//         Copyright (C) 2014-2017 Sebastian Jeckel.
+//         Copyright (C) 2020-2023 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef UREACT_ADAPTOR_SLICE_HPP
+#define UREACT_ADAPTOR_SLICE_HPP
+
+#include <type_traits>
+
+#include <ureact/adaptor/filter.hpp>
+#include <ureact/detail/take_drop_base.hpp>
+#include <ureact/type_traits.hpp>
+
+UREACT_BEGIN_NAMESPACE
+
+namespace detail
+{
+
+struct SliceAdaptor : Adaptor
+{
+    /*!
+	 * @brief Keeps first N elements from the source stream
+	 *
+	 *  Semantically equivalent of std::ranges::views::take
+	 */
+    template <typename E, typename N, class = std::enable_if_t<std::is_integral_v<N>>>
+    UREACT_WARN_UNUSED_RESULT constexpr auto operator()(
+        const events<E>& source, const N begin, const dont_deduce<N> end ) const
+    {
+        assert( begin >= 0 );
+        assert( end >= begin );
+        return filter( source,                                   //
+            [begin = countdown( begin ), end = countdown( end )] //
+            ( const auto& ) mutable {                            //
+                return end-- && !begin--; // take "end" elements, but skip "begin" of them
+            } );
+    }
+
+    template <typename N, class = std::enable_if_t<std::is_integral_v<N>>>
+    UREACT_WARN_UNUSED_RESULT constexpr auto operator()(
+        const N begin, const dont_deduce<N> end ) const
+    {
+        assert( begin >= 0 );
+        assert( end >= begin );
+        return make_partial<SliceAdaptor>( begin, end );
+    }
+};
+
+} // namespace detail
+
+/*!
+ * @brief Keeps given range of elements from the source stream
+ *
+ *  Semantically equivalent of std::ranges::views::slice
+ */
+inline constexpr detail::SliceAdaptor slice;
+
+UREACT_END_NAMESPACE
+
+#endif // UREACT_ADAPTOR_SLICE_HPP
--- tests/src/take_drop.cpp
+++ tests/src/take_drop.cpp
@@ -9,30 +9,35 @@
 #include "ureact/adaptor/collect.hpp"
 #include "ureact/adaptor/drop.hpp"
 #include "ureact/adaptor/once.hpp"
+#include "ureact/adaptor/slice.hpp"
 #include "ureact/adaptor/take.hpp"
 
 // filters that take first N elements or skip first N elements
-TEST_CASE( "TakeOrDrop" )
+TEST_CASE( "TakeDropSlice" )
 {
     ureact::context ctx;
 
     auto src = ureact::make_source<int>( ctx );
     ureact::events<int> first_n;
     ureact::events<int> without_first_n;
+    ureact::events<int> middle;
 
     SUBCASE( "Functional syntax" )
     {
         first_n = ureact::take( src, 5 );
         without_first_n = ureact::drop( src, 5 );
+        middle = ureact::slice( src, 3, 7 );
     }
     SUBCASE( "Piped syntax" )
     {
         first_n = src | ureact::take( 5 );
         without_first_n = src | ureact::drop( 5 );
+        middle = src | ureact::slice( 3, 7 );
     }
 
     const auto result_first_n = ureact::collect<std::vector>( first_n );
     const auto result_without_first_n = ureact::collect<std::vector>( without_first_n );
+    const auto result_middle = ureact::collect<std::vector>( middle );
 
     // pass integers as events
     for( int i : { 0, 1, 2, 3, 4, 5, -1, 6, 7, 8, 9 } )
@@ -43,8 +48,12 @@ TEST_CASE( "TakeOrDrop" )
         { 0, 1, 2, 3, 4 };
     const std::vector<int> expected_without_first_n = //
         /*          */ { 5, -1, 6, 7, 8, 9 };
+    const std::vector<int> expected_middle = //
+        /*    */ { 3, 4, 5, -1 };
+
     CHECK( result_first_n.get() == expected_first_n );
     CHECK( result_without_first_n.get() == expected_without_first_n );
+    CHECK( result_middle.get() == expected_middle );
 }
 
 TEST_CASE( "Once" )

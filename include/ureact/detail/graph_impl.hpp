4954d2397a680f3cd0d02c9e9e280c0e01aa835d
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-03-05 15:34:53 +0300

    refactor graph_impl.hpp so, react_graph method definitions are moved out of class and properly ordered

--- include/ureact/detail/graph_impl.hpp
+++ include/ureact/detail/graph_impl.hpp
@@ -120,15 +120,7 @@ class react_graph
 {
 public:
     react_graph() = default;
-
-    ~react_graph()
-    {
-        assert( m_node_data.empty() );
-        assert( m_scheduled_nodes.empty() );
-        assert( m_transaction_level == 0 );
-        assert( m_changed_inputs.empty() );
-        assert( m_changed_nodes.empty() );
-    }
+    ~react_graph();
 
     node_id register_node( reactive_node_interface* nodePtr );
     void unregister_node( node_id nodeId );
@@ -136,84 +128,10 @@ public:
     void attach_node( node_id nodeId, node_id parentId );
     void detach_node( node_id nodeId, node_id parentId );
 
-    void push_input( node_id nodeId )
-    {
-        m_changed_inputs.add( nodeId );
-
-        if( m_transaction_level == 0 )
-        {
-            propagate();
-        }
-    }
+    void push_input( node_id nodeId );
 
 private:
     friend class ureact::transaction;
-
-    void propagate()
-    {
-        // Fill update queue with successors of changed inputs
-        for( node_id nodeId : m_changed_inputs )
-        {
-            auto& node = m_node_data[nodeId];
-            auto* nodePtr = node.node_ptr;
-
-            const update_result result = nodePtr->update();
-
-            if( result == update_result::changed )
-            {
-                m_changed_nodes.push_back( nodePtr );
-                schedule_successors( node );
-            }
-        }
-        m_changed_inputs.clear();
-
-        // Propagate changes
-        while( m_scheduled_nodes.fetch_next() )
-        {
-            for( node_id nodeId : m_scheduled_nodes.next_values() )
-            {
-                auto& node = m_node_data[nodeId];
-                auto* nodePtr = node.node_ptr;
-
-                // A predecessor of this node has shifted to a lower level?
-                if( node.level < node.new_level )
-                {
-                    // Re-schedule this node
-                    node.level = node.new_level;
-
-                    recalculate_successor_levels( node );
-                    m_scheduled_nodes.push( nodeId, node.level );
-                    continue;
-                }
-
-                const update_result result = nodePtr->update();
-
-                // Topology changed?
-                if( result == update_result::shifted )
-                {
-                    // Re-schedule this node
-                    recalculate_successor_levels( node );
-                    m_scheduled_nodes.push( nodeId, node.level );
-                    continue;
-                }
-
-                if( result == update_result::changed )
-                {
-                    m_changed_nodes.push_back( nodePtr );
-                    schedule_successors( node );
-                }
-
-                node.queued = false;
-            }
-        }
-
-        // Cleanup buffers in changed nodes etc
-        for( reactive_node_interface* nodePtr : m_changed_nodes )
-            nodePtr->finalize();
-        m_changed_nodes.clear();
-
-        detach_queued_observers();
-    }
 
     struct node_data
     {
@@ -264,6 +182,8 @@ private:
         std::vector<entry> m_queue_data;
     };
 
+    void propagate();
+
     void recalculate_successor_levels( node_data& node );
 
     void schedule_successors( node_data& node );
@@ -280,6 +200,15 @@ private:
     std::vector<reactive_node_interface*> m_changed_nodes;
 };
 
+inline react_graph::~react_graph()
+{
+    assert( m_node_data.empty() );
+    assert( m_scheduled_nodes.empty() );
+    assert( m_transaction_level == 0 );
+    assert( m_changed_inputs.empty() );
+    assert( m_changed_nodes.empty() );
+}
+
 inline node_id react_graph::register_node( reactive_node_interface* nodePtr )
 {
     return node_id{ m_node_data.insert( node_data{ nodePtr } ) };
@@ -289,6 +218,131 @@ inline void react_graph::unregister_node( node_id nodeId )
 {
     assert( m_node_data[nodeId].successors.empty() );
     m_node_data.erase( nodeId );
+}
+
+inline void react_graph::attach_node( node_id nodeId, node_id parentId )
+{
+    auto& node = m_node_data[nodeId];
+    auto& parent = m_node_data[parentId];
+
+    parent.successors.add( nodeId );
+
+    if( node.level <= parent.level )
+    {
+        node.level = parent.level + 1;
+    }
+}
+
+inline void react_graph::detach_node( node_id nodeId, node_id parentId )
+{
+    auto& parent = m_node_data[parentId];
+    auto& successors = parent.successors;
+
+    successors.remove( nodeId );
+}
+
+inline void react_graph::push_input( node_id nodeId )
+{
+    m_changed_inputs.add( nodeId );
+
+    if( m_transaction_level == 0 )
+    {
+        propagate();
+    }
+}
+
+inline void react_graph::propagate()
+{
+    // Fill update queue with successors of changed inputs
+    for( node_id nodeId : m_changed_inputs )
+    {
+        auto& node = m_node_data[nodeId];
+        auto* nodePtr = node.node_ptr;
+
+        const update_result result = nodePtr->update();
+
+        if( result == update_result::changed )
+        {
+            m_changed_nodes.push_back( nodePtr );
+            schedule_successors( node );
+        }
+    }
+    m_changed_inputs.clear();
+
+    // Propagate changes
+    while( m_scheduled_nodes.fetch_next() )
+    {
+        for( node_id nodeId : m_scheduled_nodes.next_values() )
+        {
+            auto& node = m_node_data[nodeId];
+            auto* nodePtr = node.node_ptr;
+
+            // A predecessor of this node has shifted to a lower level?
+            if( node.level < node.new_level )
+            {
+                // Re-schedule this node
+                node.level = node.new_level;
+
+                recalculate_successor_levels( node );
+                m_scheduled_nodes.push( nodeId, node.level );
+                continue;
+            }
+
+            const update_result result = nodePtr->update();
+
+            // Topology changed?
+            if( result == update_result::shifted )
+            {
+                // Re-schedule this node
+                recalculate_successor_levels( node );
+                m_scheduled_nodes.push( nodeId, node.level );
+                continue;
+            }
+
+            if( result == update_result::changed )
+            {
+                m_changed_nodes.push_back( nodePtr );
+                schedule_successors( node );
+            }
+
+            node.queued = false;
+        }
+    }
+
+    // Cleanup buffers in changed nodes etc
+    for( reactive_node_interface* nodePtr : m_changed_nodes )
+        nodePtr->finalize();
+    m_changed_nodes.clear();
+
+    detach_queued_observers();
+}
+
+inline void react_graph::recalculate_successor_levels( node_data& node )
+{
+    for( node_id successorId : node.successors )
+    {
+        auto& successor = m_node_data[successorId];
+
+        if( successor.new_level <= node.level )
+        {
+            successor.new_level = node.level + 1;
+        }
+    }
+}
+
+inline void react_graph::schedule_successors( node_data& node )
+{
+    // add children to queue
+    for( node_id successorId : node.successors )
+    {
+        auto& successor = m_node_data[successorId];
+
+        if( !successor.queued )
+        {
+            successor.queued = true;
+            m_scheduled_nodes.push( successorId, successor.level );
+        }
+    }
 }
 
 UREACT_WARN_UNUSED_RESULT inline bool react_graph::topological_queue::fetch_next()
@@ -326,55 +380,6 @@ UREACT_WARN_UNUSED_RESULT inline bool react_graph::topological_queue::fetch_next
     m_queue_data.resize( to_resize );
 
     return !m_next_data.empty();
-}
-
-inline void react_graph::attach_node( node_id nodeId, node_id parentId )
-{
-    auto& node = m_node_data[nodeId];
-    auto& parent = m_node_data[parentId];
-
-    parent.successors.add( nodeId );
-
-    if( node.level <= parent.level )
-    {
-        node.level = parent.level + 1;
-    }
-}
-
-inline void react_graph::detach_node( node_id nodeId, node_id parentId )
-{
-    auto& parent = m_node_data[parentId];
-    auto& successors = parent.successors;
-
-    successors.remove( nodeId );
-}
-
-inline void react_graph::schedule_successors( node_data& node )
-{
-    // add children to queue
-    for( node_id successorId : node.successors )
-    {
-        auto& successor = m_node_data[successorId];
-
-        if( !successor.queued )
-        {
-            successor.queued = true;
-            m_scheduled_nodes.push( successorId, successor.level );
-        }
-    }
-}
-
-inline void react_graph::recalculate_successor_levels( node_data& node )
-{
-    for( node_id successorId : node.successors )
-    {
-        auto& successor = m_node_data[successorId];
-
-        if( successor.new_level <= node.level )
-        {
-            successor.new_level = node.level + 1;
-        }
-    }
 }
 
 class context_internals

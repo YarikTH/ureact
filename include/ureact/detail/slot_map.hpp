5e5123ddf86b267fdcd42609f15c22c7017b8e8c
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-03-19 08:56:59 +0300

    extract free indices related code from slot_map to slot_map::free_indices

--- include/ureact/detail/slot_map.hpp
+++ include/ureact/detail/slot_map.hpp
@@ -88,14 +88,14 @@ public:
         // If we erased something other than the last element, save in free index list.
         if( index != ( total_size() - 1 ) )
         {
-            m_free_indices[m_free_size++] = index;
+            m_free_indices.push( index );
         }
 
         destroy_at( index );
         --m_size;
 
         // If free indices appeared at the end of allocated range, remove them from list
-        shake_free_indices();
+        m_free_indices.shake( m_size );
     }
 
     /// Return if there is any element inside
@@ -111,10 +111,8 @@ public:
         size_type index = 0;
 
         // Skip over sorted free indices.
-        for( size_type j = 0; j < m_free_size; ++j )
+        for( size_type free_index : m_free_indices )
         {
-            size_type free_index = m_free_indices[j];
-
             for( ; index < size; ++index )
             {
                 if( index == free_index )
@@ -136,7 +134,7 @@ public:
         }
 
         m_size = 0;
-        m_free_size = 0;
+        m_free_indices.clear();
     }
 
     /// Clear the data and return container to its initial state with 0 capacity
@@ -151,6 +149,92 @@ public:
     }
 
 private:
+    class free_indices
+    {
+    public:
+        free_indices() = default;
+
+        explicit free_indices( const size_type amount )
+            : m_data{ std::make_unique<size_type[]>( amount ) }
+        {}
+
+        void reset()
+        {
+            m_data.reset();
+        }
+
+        UREACT_WARN_UNUSED_RESULT size_type amount() const
+        {
+            return m_size;
+        }
+
+        UREACT_WARN_UNUSED_RESULT bool empty() const
+        {
+            return m_size == 0;
+        }
+
+        void clear()
+        {
+            m_size = 0;
+        }
+
+        void push( const size_type index )
+        {
+            m_data[m_size++] = index;
+            detail::sort( begin_(), end_() );
+        }
+
+        UREACT_WARN_UNUSED_RESULT size_type pop()
+        {
+            return m_data[--m_size];
+        }
+
+        void shake( const size_type elements )
+        {
+            const auto total_size = [&]() { return elements + m_size; };
+
+            while( m_size > 0 && back_() == total_size() - 1 )
+            {
+                --m_size;
+            }
+        }
+
+        UREACT_WARN_UNUSED_RESULT bool have( const size_type index ) const
+        {
+            const auto it = detail::find( begin(), end(), index );
+            return it != end();
+        }
+
+        UREACT_WARN_UNUSED_RESULT const size_type* begin() const
+        {
+            return &m_data[0];
+        }
+
+        UREACT_WARN_UNUSED_RESULT const size_type* end() const
+        {
+            return &m_data[m_size];
+        }
+
+    private:
+        UREACT_WARN_UNUSED_RESULT const size_type& back_() const
+        {
+            return m_data[m_size - 1];
+        }
+
+        UREACT_WARN_UNUSED_RESULT size_type* begin_()
+        {
+            return &m_data[0];
+        }
+
+        UREACT_WARN_UNUSED_RESULT size_type* end_()
+        {
+            return &m_data[m_size];
+        }
+
+        std::unique_ptr<size_type[]> m_data;
+        size_type m_size = 0u;
+    };
+
     static inline constexpr size_t initial_capacity = 8;
     static inline constexpr size_t grow_factor = 2;
 
@@ -163,7 +247,7 @@ private:
 
     UREACT_WARN_UNUSED_RESULT bool has_free_indices() const
     {
-        return m_free_size > 0;
+        return !m_free_indices.empty();
     }
 
     UREACT_WARN_UNUSED_RESULT size_type calculate_next_capacity() const
@@ -173,36 +257,12 @@ private:
 
     UREACT_WARN_UNUSED_RESULT size_type total_size() const
     {
-        return m_size + m_free_size;
-    }
-
-    UREACT_WARN_UNUSED_RESULT bool is_free_index( const size_type index ) const
-    {
-        const auto begin_ = &m_free_indices[0];
-        const auto end_ = &m_free_indices[m_free_size];
-
-        const auto it = detail::find( begin_, end_, index );
-        return it != end_;
+        return m_size + m_free_indices.amount();
     }
 
     UREACT_WARN_UNUSED_RESULT bool has_index( const size_type index ) const
     {
-        return index < total_size() && !is_free_index( index );
-    }
-
-    void shake_free_indices()
-    {
-        if( m_free_size == 0 )
-        {
-            return;
-        }
-
-        detail::sort( &m_free_indices[0], &m_free_indices[m_free_size] );
-
-        while( m_free_size > 0 && m_free_indices[m_free_size - 1] == total_size() - 1 )
-        {
-            --m_free_size;
-        }
+        return index < total_size() && !m_free_indices.have( index );
     }
 
     void grow()
@@ -211,7 +271,6 @@ private:
         const size_type new_capacity = calculate_next_capacity();
 
         auto new_data = std::make_unique<storage_type[]>( new_capacity );
-        auto new_free_indices = std::make_unique<size_type[]>( new_capacity );
 
         // Move data to new storage
         for( size_type i = 0; i < m_capacity; ++i )
@@ -225,7 +284,7 @@ private:
 
         // Use new storage
         m_data = std::move( new_data );
-        m_free_indices = std::move( new_free_indices );
+        m_free_indices = free_indices{ new_capacity };
         m_capacity = new_capacity;
     }
 
@@ -237,7 +296,7 @@ private:
 
     size_type insert_at_freed_slot( value_type&& value )
     {
-        const size_type next_free_index = m_free_indices[--m_free_size];
+        const size_type next_free_index = m_free_indices.pop();
         construct_at( next_free_index, std::move( value ) );
         ++m_size;
 
@@ -261,10 +320,9 @@ private:
     }
 
     std::unique_ptr<storage_type[]> m_data;
-    std::unique_ptr<size_type[]> m_free_indices;
+    free_indices m_free_indices;
 
     size_type m_size = 0;
-    size_type m_free_size = 0;
     size_type m_capacity = 0;
 };
 

8a482544d5527ca251c8c65845c0bfacc8580d17
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-08-16 02:11:34 +0400

    add enumerate adaptor

---
+++ include/ureact/adaptor/enumerate.hpp
@@ -0,0 +1,45 @@
+//
+//         Copyright (C) 2014-2017 Sebastian Jeckel.
+//         Copyright (C) 2020-2023 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef UREACT_ADAPTOR_ENUMERATE_HPP
+#define UREACT_ADAPTOR_ENUMERATE_HPP
+
+#include <tuple>
+
+#include <ureact/adaptor/transform.hpp>
+
+UREACT_BEGIN_NAMESPACE
+
+namespace detail
+{
+
+struct EnumerateAdaptorClosure : AdaptorClosure
+{
+    template <typename E>
+    UREACT_WARN_UNUSED_RESULT constexpr auto operator()( const events<E>& source ) const
+    {
+        return transform( source,                            //
+            [index = size_t{ 0 }]( const auto& e ) mutable { //
+                return std::make_tuple( index++, e );
+            } );
+    }
+};
+
+} // namespace detail
+
+/*!
+ * @brief Takes event stream and creates a new event stream with a tuple of both the element's position and its value
+ * 
+ *  For every event e in source, emit std::tuple<size_t, E>(index++, e).
+ */
+inline constexpr detail::EnumerateAdaptorClosure enumerate;
+
+UREACT_END_NAMESPACE
+
+#endif //UREACT_ADAPTOR_ENUMERATE_HPP
---
+++ tests/src/adaptor/enumerate.cpp
@@ -0,0 +1,46 @@
+//
+//         Copyright (C) 2020-2023 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+#include "ureact/adaptor/enumerate.hpp"
+
+#include <tuple>
+#include <vector>
+
+#include "catch2_extra.hpp"
+#include "ureact/adaptor/collect.hpp"
+#include "ureact/events.hpp"
+
+// Based on example on https://en.cppreference.com/w/cpp/ranges/enumerate_view
+TEST_CASE( "ureact::enumerate" )
+{
+    ureact::context ctx;
+
+    auto src = ureact::make_source<char>( ctx );
+    ureact::events<std::tuple<size_t, char>> enumerated;
+
+    SECTION( "Functional syntax" )
+    {
+        enumerated = ureact::enumerate( src );
+    }
+    SECTION( "Piped syntax" )
+    {
+        enumerated = src | ureact::enumerate;
+    }
+
+    const auto result = ureact::collect<std::vector>( enumerated );
+
+    for( char c : { 'A', 'B', 'C', 'D' } )
+        src << c;
+
+    CHECK( result.get()
+           == std::vector<std::tuple<size_t, char>>{
+               { 0, 'A' },
+               { 1, 'B' },
+               { 2, 'C' },
+               { 3, 'D' },
+           } );
+}

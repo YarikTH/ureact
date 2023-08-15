b0c535441f61c8b94141b57485dfd508afb4af35
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-08-15 17:44:30 +0400

    extract ureact::detail::event_zip_node into ureact/detail/zip_base.hpp

--- include/ureact/adaptor/zip.hpp
+++ include/ureact/adaptor/zip.hpp
@@ -10,88 +10,13 @@
 #ifndef UREACT_ADAPTOR_ZIP_HPP
 #define UREACT_ADAPTOR_ZIP_HPP
 
-#include <deque>
-
 #include <ureact/detail/adaptor.hpp>
-#include <ureact/events.hpp>
+#include <ureact/detail/zip_base.hpp>
 
 UREACT_BEGIN_NAMESPACE
 
 namespace detail
 {
-
-template <typename... Values>
-class event_zip_node final : public event_stream_node<std::tuple<Values...>>
-{
-public:
-    explicit event_zip_node( const context& context, const events<Values>&... sources )
-        : event_zip_node::event_stream_node( context )
-        , m_slots( sources... )
-    {
-        this->attach_to( sources... );
-    }
-
-    ~event_zip_node() override
-    {
-        this->detach_from_all();
-    }
-
-    UREACT_WARN_UNUSED_RESULT update_result update() override
-    {
-        // Move events into buffers
-        // TODO: move to method
-        std::apply(
-            []( slot<Values>&... slots ) {
-                ( fetch_buffer( slots ), ... ); //
-            },
-            m_slots );
-
-        // TODO: move to method
-        const auto is_ready = [this]() {
-            return std::apply(
-                []( const slot<Values>&... slots ) {
-                    return ( !slots.buffer.empty() && ... ); //
-                },
-                m_slots );
-        };
-
-        while( is_ready() )
-        {
-            // Pop values from buffers and emit tuple
-            // TODO: move to method
-            std::apply(
-                [this]( slot<Values>&... slots ) {
-                    this->get_events().emplace_back( slots.buffer.front()... );
-                    ( slots.buffer.pop_front(), ... );
-                },
-                m_slots );
-        }
-
-        return !this->get_events().empty() ? update_result::changed : update_result::unchanged;
-    }
-
-private:
-    template <typename T>
-    struct slot
-    {
-        explicit slot( const events<T>& source )
-            : source( source )
-        {}
-
-        events<T> source;
-        std::deque<T> buffer;
-    };
-
-    template <typename T>
-    static void fetch_buffer( slot<T>& slot )
-    {
-        const auto& src_events = get_internals( slot.source ).get_events();
-
-        slot.buffer.insert( slot.buffer.end(), src_events.begin(), src_events.end() );
-    }
-
-    std::tuple<slot<Values>...> m_slots;
-};
 
 struct ZipAdaptor : Adaptor
 {

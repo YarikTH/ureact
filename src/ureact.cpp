414d7a2e89e65a910bbf870e27023b24c313ebf8
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2023-08-26 16:55:14 +0400

    WIP add separate ureact target for static/shared library form of ureact, while renaming header only form as ureact-header-only

---
+++ src/ureact.cpp
@@ -0,0 +1,15 @@
+//
+//         Copyright (C) 2014-2017 Sebastian Jeckel.
+//         Copyright (C) 2020-2023 Krylov Yaroslav.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <ureact/detail/context.inl>
+#include <ureact/detail/graph_impl.inl>
+#include <ureact/detail/graph_interface.inl>
+#include <ureact/detail/node_base.inl>
+#include <ureact/detail/observer.inl>
+#include <ureact/detail/transaction.inl>

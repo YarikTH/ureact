de9229dea067f7c8a8ca2b82945f330e12dbb533
Krylov Yaroslav <krylov.yaroslav@gmail.com>
2024-01-06 23:48:18 +0100

    Add partial umbrella header "ureact/ureact.hpp"
    It includes everything except adaptors.

---
+++ include/ureact/ureact.hpp
@@ -0,0 +1,30 @@
+//
+//         Copyright (C) 2014-2017 Sebastian Jeckel.
+//         Copyright (C) 2020-2024 Yaroslav Krylov.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef UREACT_UREACT_HPP
+#define UREACT_UREACT_HPP
+
+// clang-format off
+#include "ureact/version.hpp"
+// clang-format on
+
+#include <ureact/context.hpp>
+#include <ureact/events.hpp>
+#include <ureact/observer.hpp>
+#include <ureact/signal.hpp>
+#include <ureact/transaction.hpp>
+#include <ureact/utility/event_emitter.hpp>
+#include <ureact/utility/event_range.hpp>
+#include <ureact/utility/observe_policy.hpp>
+#include <ureact/utility/observer_action.hpp>
+#include <ureact/utility/signal_pack.hpp>
+#include <ureact/utility/type_traits.hpp>
+#include <ureact/utility/unit.hpp>
+
+#endif //UREACT_UREACT_HPP
